5e40987804ade211dfc29d323b772d55b645fb0f
Vien Pham <bfb9jf@virginia.edu>
2025-07-28 03:02:03 -0400

    test units rewritten. Some bug fixes.

---
+++ jest.config.mjs
@@ -0,0 +1,5 @@
+export default {
+  testEnvironment: 'node',
+  transform: {}, // disable babel transform if you don't use it
+  testMatch: ['<rootDir>/public/ChessAI/test/**/*.js'], // <- use <rootDir> for root folder
+};
